b40efd2eea58526bcb8addf5ac02e32c3c1687db
Pranav Salunke <dguitarbite@gmail.com>
2016-12-21 16:29:12 +0100

    minor commit: Fixing import order in setup.py.

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from setuptools import setup
 import libvirt_provider
+from setuptools import setup
 
 with open('README.rst') as readme_file:
     readme = readme_file.read()
